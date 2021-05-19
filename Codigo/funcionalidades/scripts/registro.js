7dffbd466560c687ef92b37f33852b41bba1a4f7
Arthur Wilson <83772827+arthurwilson2021@users.noreply.github.com>
2021-05-19 11:37:48 -0300

    Created 3 functions for: 'Validate UserName', 'Enable RegisterButton' and 'Disable RegisterButton'

--- Codigo/funcionalidades/scripts/registro.js
+++ Codigo/funcionalidades/scripts/registro.js
@@ -1,15 +1,42 @@
 onload = () => {
 
-
-
-
-
-
-
-    register_button.onclick = () => {
-        console.log("Clicou! Funcionou!");
+    // REGISTER BUTTON FUNCTIONS
+    function enable_registerButton(){
+        register_button.removeAttribute('disabled');
+        register_button.style.background = '#007bff';
+        register_button.style.border = '#007bff'
     }
 
+    function disable_registerButton(){
+        register_button.setAttribute('disabled', true);
+        register_button.style.background = 'gray';
+        register_button.style.border = '#3a3a3a'
+    }
+
+    // VALIDATE FUNCTIONS
+    function validate_userName(userName){
+        if (userName.value.length < 3)
+        {
+            //console.log("Nome inválido!");
+            disable_registerButton();
+        }
+        else{
+            //console.log("Nome válido!");
+            enable_registerButton();
+        }
+    }
+
+    disable_registerButton();
+
+    user_name.onchange = () =>{
+        validate_userName(user_name);
+    }
+
+    /*
+    register_button.onclick = () =>{
+        console.log("Clicou! Funcionou!");
+    }
+    */
 
 
 
