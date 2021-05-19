9b9722718ab6226ff8de896e7650e95f679679d6
PeterCcT <pedrorodriguesfereira@gmail.com>
2021-05-19 00:25:19 -0300

    Login functionality completed

---
+++ Codigo/funcionalidades/scripts/login.js
@@ -0,0 +1,22 @@
+import { findUserByPropertieAndValue } from './find_user.js'
+window.onload = () => {
+    loginForm.onsubmit = (ev) => {
+        try {
+            ev.preventDefault()
+
+            const [user] = findUserByPropertieAndValue({
+                'email': userEmail.value,
+                'password': userPassword.value
+            })
+
+            if (user)
+                window.location.href = '../Main/wwa.html'
+            else
+                alert('Usuário não encontrado!\nEmail ou senha incorretos')
+
+        } catch (error) {
+            console.error(error)
+            alert('Erro ao efeturar login, tente novamente mais tarde')
+        }
+    }
+}
