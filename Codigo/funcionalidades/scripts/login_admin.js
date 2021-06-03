6cb3ab738a4ee7a5b0a5e1da583bf012882c1b67
Arthur Wilson <83772827+arthurwilson2021@users.noreply.github.com>
2021-06-03 20:49:20 -0300

    Activate and deactivate the button is working

--- Codigo/funcionalidades/scripts/login_admin.js
+++ Codigo/funcionalidades/scripts/login_admin.js
@@ -8,21 +8,37 @@ window.onload = () => {
         }]
     };
 
-        // LOGIN_BUTTON FUNCTIONS
-        function enable_loginButton(){
-            login_button.removeAttribute('disabled');
-            login_button.style.background = '#007bff';
-            login_button.style.border = '#007bff'
-        }
-    
-        function disable_loginButton(){
-            login_button.setAttribute('disabled', true);
-            login_button.style.background = 'gray';
-            login_button.style.border = '#3a3a3a'
-        }
+    // LOGIN_BUTTON FUNCTIONS
+    function enable_loginButton(){
+        login_button.removeAttribute('disabled');
+        login_button.style.background = '#007bff';
+        login_button.style.border = '#007bff'
+    }
 
-        disable_loginButton();
+    function disable_loginButton(){
+        login_button.setAttribute('disabled', true);
+        login_button.style.background = 'gray';
+        login_button.style.border = '#3a3a3a'
+    }
 
-        console.log("Dado: " + db.admin[0].password);
+    disable_loginButton();
+
+    userEmail.oninput = () => {
+        if( userEmail.value != '' && user_password.value != ''){
+            enable_loginButton();
+        }
+        else{
+            disable_loginButton();
+        }
+    }
+
+    user_password.oninput = () => {
+        if( userEmail.value != '' && user_password.value != ''){
+            enable_loginButton();
+        }
+        else{
+            disable_loginButton();
+        }
+    }
 
 }
